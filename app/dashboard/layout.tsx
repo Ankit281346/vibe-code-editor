ef577aa751bc72ee5687d432b9fc0abd0332bc06
Ankit <Ankit7@Ankit.com>
2026-06-30 23:55:56 +0530

    Remove unused ts-expect-error directive in dashboard layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -35,7 +35,6 @@ export default async function DashboardLayout({
     
     <div className="flex min-h-screen w-full overflow-x-hidden">
       {/* Dashboard Sidebar */}
-      {/* @ts-expect-error */}
       <DashboardSidebar initialPlaygroundData={formattedPlaygroundData}/>
       <main className="flex-1">{children}</main>
     </div>
